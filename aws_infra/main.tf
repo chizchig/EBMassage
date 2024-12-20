fd98a8c95699fa079080b16a4291f93c2146b3aa
Chigaemezu Dennar <salientcrossing@outlook.com>
2024-12-20 13:21:59 -0700

    storming 60

--- aws_infra/main.tf
+++ aws_infra/main.tf
@@ -3,127 +3,127 @@ data "aws_availability_zones" "available" {
 }
 
 
-# module "vpc" {
-#   source = "./modules/vpc"
+module "vpc" {
+  source = "./modules/vpc"
 
-#   name             = var.name
-#   vpc_cidr_block   = var.vpc_cidr_block
-#   azs              = local.az_names
-#   public_subnets   = local.public_subnets
-#   private_subnets  = local.private_subnets
-#   database_subnets = local.database_subnets
+  name             = var.name
+  vpc_cidr_block   = var.vpc_cidr_block
+  azs              = local.az_names
+  public_subnets   = local.public_subnets
+  private_subnets  = local.private_subnets
+  database_subnets = local.database_subnets
 
-#   enable_nat_gateway = false
-#   enable_vpn_gateway = true
+  enable_nat_gateway = false
+  enable_vpn_gateway = true
 
-#   tags = {
-#     Name = "${var.name}_vpc"
-#   }
-# }
+  tags = {
+    Name = "${var.name}_vpc"
+  }
+}
 
-# module "eks" {
-#   source = "./modules/eks"
+module "eks" {
+  source = "./modules/eks"
 
-#   cluster_name    = var.cluster_name
-#   cluster_version = var.cluster_version
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
 
-#   # VPC Configuration
-#   # vpc_id          = module.vpc.vpc_id
-#   public_subnets  = module.vpc.public_subnets
-#   # private_subnets = module.vpc.private_subnets
+  # VPC Configuration
+  # vpc_id          = module.vpc.vpc_id
+  public_subnets  = module.vpc.public_subnets
+  # private_subnets = module.vpc.private_subnets
 
-#   # Node groups configuration
-#   node_groups     = var.node_groups
+  # Node groups configuration
+  node_groups     = var.node_groups
 
-#   # IAM roles configuration
-#   roles           = var.roles
+  # IAM roles configuration
+  roles           = var.roles
 
-#   # Tags
-#   # tags = {
-#   #   Environment = var.environment
-#   #   Project     = var.project_name
-#   # }
-# }
+  # Tags
+  # tags = {
+  #   Environment = var.environment
+  #   Project     = var.project_name
+  # }
+}
 
-# module "security_group" {
-#   source = "./modules/security_group"
+module "security_group" {
+  source = "./modules/security_group"
 
-#   vpc_id          = module.vpc.vpc_id
-#   security_groups = var.security_groups
-# }
+  vpc_id          = module.vpc.vpc_id
+  security_groups = var.security_groups
+}
 
-# module "rds" {
-#   source = "./modules/rds"
+module "rds" {
+  source = "./modules/rds"
 
-#   rds_instances = {
-#     for k, v in var.rds_instances : k => merge(v, {
-#       vpc_security_group_ids = values(module.security_group.security_group_ids)  # Use all security group IDs
+  rds_instances = {
+    for k, v in var.rds_instances : k => merge(v, {
+      vpc_security_group_ids = values(module.security_group.security_group_ids)  # Use all security group IDs
       
 
-#     })
-#   }
+    })
+  }
 
-#   private_subnet_ids = values(module.vpc.private_subnet_ids)  # Convert map to list
+  private_subnet_ids = values(module.vpc.private_subnet_ids)  # Convert map to list
 
-#   subnet_group = {
-#     name       = "${var.name}-db-subnet-group"
-#     subnet_ids = coalesce(var.subnet_group.subnet_ids, values(module.vpc.database_subnet_ids))
-#     tags       = {
-#       Name = "${var.name}-db-subnet-group"
-#     }
-#   }
-# }
+  subnet_group = {
+    name       = "${var.name}-db-subnet-group"
+    subnet_ids = coalesce(var.subnet_group.subnet_ids, values(module.vpc.database_subnet_ids))
+    tags       = {
+      Name = "${var.name}-db-subnet-group"
+    }
+  }
+}
 
-# data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {}
 
-# # Create an Amazon ECR repository
-# resource "aws_ecr_repository" "cloud_index" {
-#   name                 = var.ecr_repository_name
-#   image_tag_mutability = var.image_tag_mutability
+# Create an Amazon ECR repository
+resource "aws_ecr_repository" "cloud_index" {
+  name                 = var.ecr_repository_name
+  image_tag_mutability = var.image_tag_mutability
 
-#   # Enable image scanning on push
-#   image_scanning_configuration {
-#     scan_on_push = var.scan_on_push
-#   }
+  # Enable image scanning on push
+  image_scanning_configuration {
+    scan_on_push = var.scan_on_push
+  }
 
-#   # Encrypt the repository
-#   encryption_configuration {
-#     encryption_type = var.encryption_type
-#     kms_key         = var.kms_key_arn
-#   }
+  # Encrypt the repository
+  encryption_configuration {
+    encryption_type = var.encryption_type
+    kms_key         = var.kms_key_arn
+  }
 
-#   tags = merge(
-#     var.tags,
-#     {
-#       Name      = var.ecr_repository_name
-#       ManagedBy = "Terraform"
-#       AccountID = data.aws_caller_identity.current.account_id
-#     }
-#   )
-# }
+  tags = merge(
+    var.tags,
+    {
+      Name      = var.ecr_repository_name
+      ManagedBy = "Terraform"
+      AccountID = data.aws_caller_identity.current.account_id
+    }
+  )
+}
 
-# # Create a lifecycle policy for the ECR repository
-# resource "aws_ecr_lifecycle_policy" "cloud_index" {
-#   repository = aws_ecr_repository.cloud_index.name
+# Create a lifecycle policy for the ECR repository
+resource "aws_ecr_lifecycle_policy" "cloud_index" {
+  repository = aws_ecr_repository.cloud_index.name
 
-#   policy = jsonencode({
-#     rules = [
-#       {
-#         rulePriority = 1
-#         description  = "Expire untagged images older than ${var.untagged_image_expiration_days} days"
-#         selection = {
-#           tagStatus   = "untagged"
-#           countType   = "sinceImagePushed"
-#           countUnit   = "days"
-#           countNumber = var.untagged_image_expiration_days
-#         }
-#         action = {
-#           type = "expire"
-#         }
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Expire untagged images older than ${var.untagged_image_expiration_days} days"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = var.untagged_image_expiration_days
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
 
 resource "kubernetes_manifest" "deployment" {
   manifest = yamldecode(file("../aws_infra/k8s/deployment.yml"))
