da49f233b87e9fe9c6a344be1c935928af806637
Chigaemezu Dennar <salientcrossing@outlook.com>
2024-12-22 17:01:26 -0700

    storming 60

--- aws_infra/main.tf
+++ aws_infra/main.tf
@@ -131,9 +131,9 @@ resource "null_resource" "k8s_deploy" {
   provisioner "local-exec" {
   command = <<EOT
     aws eks update-kubeconfig --name falcon-eks-cluster --region us-east-1 &&
-    kubectl apply -f ./k8s/deployment.yaml &&
-    kubectl apply -f ./k8s/service.yaml &&
-    kubectl apply -f ./k8s/ingress.yaml
+    kubectl apply -f ./k8s/deployment.yml &&
+    kubectl apply -f ./k8s/service.yml &&
+    kubectl apply -f ./k8s/ingress.yml
   EOT
  }
 
